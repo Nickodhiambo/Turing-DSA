951c753520ad6b37abde5b73d59bbed75077f163
Nick <nodhiambo01@gmail.com>
2026-02-04 11:18:47 +0300

    Use backtracking to generate all permutations of a list

---
+++ backtracking/all_permutations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+def permutations(arr: list[int]) -> list[list[int]]:
+    result = []
+    def backtrack(i):
+        if i == len(arr): # Base case: Full permutation formed
+            result.append(arr[:])
+            return
+        for j in range(i, len(arr)):
+            arr[i], arr[j] = arr[j], arr[i]
+            backtrack(i+1)
+            arr[i], arr[j] = arr[j], arr[i]
+    backtrack(0)
+    return result
+
+if __name__ == '__main__':
+   print( permutations([1,2,3]))
